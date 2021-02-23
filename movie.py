68a0a0f0a3d34df21d8dbb46beacbbfa5b708ad7
Hyper-glitch <54985705+Hyper-glitch@users.noreply.github.com>
2021-02-23 21:08:37 +0300

    Update movie.py

--- movie.py
+++ movie.py
@@ -4,7 +4,9 @@ from movie.view import View
 
 def main():
     view = View()
+    print("Here are empty list of movies")
     view.show_all()
+    print("Please add the first movie")
     movie_to_add = Movie(
         title="The Imitation Game",
         date_released=2014,
@@ -16,6 +18,20 @@ def main():
     view.add_movie(movie_obj=movie_to_add)
     view.show_all()
 
+    movie_to_add_two = Movie(
+        title="Solaris",
+        date_released=1972,
+        actors=[
+            Actor(name="Donatas", last_name="Banionis",
+                  role="Kris Kelvin"),
+            Actor(name="Natalya", last_name="Bondarchuk",
+                  role="Hari")
+        ]
+    )
+    view.add_movie(movie_obj=movie_to_add_two)
+    view.show_all()
 
+    
 if __name__ == '__main__':
     main()
+    
